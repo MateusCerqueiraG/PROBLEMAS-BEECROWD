31f9504b4964d64dd9371f66158418e1e3b0203a
Mateus C <129126812+MateusCerqueiraG@users.noreply.github.com>
2023-08-29 11:52:52 -0300

    PRODUTO SIMPLES

---
+++ PRODUTO SIMPLES.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () {
+    
+    int PROD;
+    int numero1;
+    int numero2;
+    
+    scanf("%d", &numero1);
+    scanf("%d", &numero2);
+    
+    PROD = numero1 * numero2;
+    
+    printf("PROD = %d\n", PROD);
+    
+    return 0;
+}
